de149dddde0f072f640a56f60e00d7cd28101bbf
Nikita Puzyrev <kutase123@gmail.com>
2017-05-19 18:48:32 +0500

    fix Restart func

--- gameLoop.go
+++ gameLoop.go
@@ -6,17 +6,17 @@ import (
 )
 
 type GameLoop struct {
-	onUpdate  func(float64)
-	tickRate  time.Duration
-	canUpdate bool
+	onUpdate func(float64)
+	tickRate time.Duration
+	quit     chan bool
 }
 
 // Create new game loop
 func New(tickRate time.Duration, onUpdate func(float64)) *GameLoop {
 	return &GameLoop{
-		onUpdate:  onUpdate,
-		tickRate:  tickRate,
-		canUpdate: false,
+		onUpdate: onUpdate,
+		tickRate: tickRate,
+		quit:     make(chan bool),
 	}
 }
 
@@ -27,47 +27,45 @@ func (gl *GameLoop) startLoop() {
 	tickInterval := time.Second / gl.tickRate
 	timeStart := time.Now().UnixNano()
 
-	tick := time.Tick(tickInterval)
+	ticker := time.NewTicker(tickInterval)
 
 	for {
-		if !gl.canUpdate {
-			break
-		}
-
 		select {
-		case <-tick:
+		case <-ticker.C:
 			now := time.Now().UnixNano()
 			// DT in seconds
 			delta := float64(now-timeStart) / 1000000000
 			timeStart = now
 			gl.onUpdate(delta)
+
+		case <-gl.quit:
+			ticker.Stop()
 		}
 	}
 }
 
-func (gl *GameLoop) GetTickRate () time.Duration {
+func (gl *GameLoop) GetTickRate() time.Duration {
 	return gl.tickRate
 }
 
 // Set tickRate and restart game loop
-func (gl *GameLoop) SetTickRate (tickRate time.Duration) {
+func (gl *GameLoop) SetTickRate(tickRate time.Duration) {
 	gl.tickRate = tickRate
 	gl.Restart()
 }
 
 // Start game loop
 func (gl *GameLoop) Start() {
-	gl.canUpdate = true
 	go gl.startLoop()
 }
 
 // Stop game loop
 func (gl *GameLoop) Stop() {
-	gl.canUpdate = false
+	gl.quit <- true
 }
 
 // Restart game loop
-func (gl *GameLoop) Restart () {
+func (gl *GameLoop) Restart() {
 	gl.Stop()
 	gl.Start()
 }
